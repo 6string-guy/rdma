3aa7f74795c530bf33a3e6b3bfbebc672212533b
Sanskar <sanskarjaiswal010@gmail.com>
2025-07-20 22:52:14 +0530

    header file

--- server.c
+++ server.c
@@ -1,62 +1,107 @@
+#include "header.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <infiniband/verbs.h>
-#include <rdma/rdma_cma.h>
-#include "header.h"
-#include <netinet/in.h>
 #include <arpa/inet.h>
 
+int main() {
+    struct rdma_event_channel *ec = rdma_create_event_channel();
+    struct rdma_cm_id *listen_id;
+    rdma_create_id(ec, &listen_id, NULL, RDMA_PS_TCP);
 
-int main()
-{
-    // sare device le aao
-    struct ibv_device **device_list;
-    ibv_get_device_list(device_list);
-    // device free kar lo if occupied
-    ibv_free_device_list(device_list);
-    struct ibv_context *context = ibv_open_device(device_list[0]);
-
-    struct ibv_pd *pd = ibv_alloc_pd(context);
-
-    // in case of failed operation NULL milega
-    struct ibv_cq *cq = ibv_create_cq(context, 30, NULL, NULL, 0);
-    char *buffer = malloc(1024);
-    struct ibv_mr *mr = ibv_reg_mr(pd, buffer, 1024, IBV_ACCESS_LOCAL_WRITE | IBV_ACCESS_REMOTE_READ);
-
-    struct ibv_qp_init_attr qp_attr = {
-        .send_cq = cq, // why same
-        .recv_cq = cq, // why same
-        .cap = {
-            .max_send_wr = 10,
-            .max_recv_wr = 10,
-            .max_send_sge = 5, // logic behind setting it to 5
-            .max_recv_sge = 5  // logic behind setting it to 5
-
-        },
-        .qp_type = IBV_QPT_RC,
+    // Bind to SoftRoCE interface IP and port
+    struct sockaddr_in addr = {
+        .sin_family = AF_INET,
+        .sin_port   = htons(18515)
     };
+    inet_pton(AF_INET, "192.168.56.101", &addr.sin_addr);
+    rdma_bind_addr(listen_id, (struct sockaddr *)&addr);
+    rdma_listen(listen_id, 1);
+    printf("Server listening on %s:%d\n", "192.168.56.101", 18515);
 
-    struct ibv_qp *qp = ibv_create_qp(pd, &qp_attr);
-    struct rdma_event_channel *channel = rdma_create_event_channel();
-    struct rdma_cm_id *cm_id;
-    rdma_create_id(channel, &cm_id, NULL, RDMA_PS_TCP);
-    struct sockaddr_in server_addr;
+    // Wait for a connection request
+    struct rdma_cm_event *event;
+    rdma_get_cm_event(ec, &event);
+    struct rdma_cm_id *conn_id = event->id;
+    rdma_ack_cm_event(event);
 
-    memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(18515);                       // Choose your port
-    inet_pton(AF_INET, "192.168.56.101", &server_addr.sin_addr); // Replace with your eth0's IP
+    // Allocate RDMA resources
+    struct ibv_pd *pd = ibv_alloc_pd(conn_id->verbs);
+    struct ibv_cq *cq = ibv_create_cq(conn_id->verbs, 1, NULL, NULL, 0);
+    struct ibv_qp_init_attr qp_attr = {
+        .send_cq = cq, .recv_cq = cq,
+        .cap     = { .max_send_wr = 1, .max_recv_wr = 1,
+                     .max_send_sge = 1, .max_recv_sge = 1 },
+        .qp_type = IBV_QPT_RC
+    };
+    rdma_create_qp(conn_id, pd, &qp_attr);
 
-    rdma_bind_addr(cm_id, (struct sockaddr *)&server_addr);
-    rdma_listen(cm_id, 1);
+    // Allocate and register buffer
+    void *buf = malloc(BUF_SIZE);
+    struct ibv_mr *mr = ibv_reg_mr(pd, buf, BUF_SIZE,
+        IBV_ACCESS_LOCAL_WRITE | IBV_ACCESS_REMOTE_WRITE);
 
-    // aab memory ko deregister kar do
-    // int a = ibv_dereg_mr(mr);
-    // if (a != 0)
-    // {
-    //     printf("Failed to degegister memory error no. %d\n ", a);
-    // }
+    // Accept the connection
+    struct rdma_conn_param conn_param = { 0 };
+    rdma_accept(conn_id, &conn_param);
+    rdma_get_cm_event(ec, &event);
+    rdma_ack_cm_event(event);
+    printf("Client connected\n");
 
+    // Post a receive for the incoming request
+    struct ibv_sge sge = { .addr = (uintptr_t)buf,
+                           .length = sizeof(struct calc_req),
+                           .lkey = mr->lkey };
+    struct ibv_recv_wr recv_wr = { .wr_id = 1, .sg_list = &sge,
+                                   .num_sge = 1 };
+    struct ibv_recv_wr *bad_recv;
+    ibv_post_recv(conn_id->qp, &recv_wr, &bad_recv);
+
+    // Poll for the request
+    struct ibv_wc wc;
+    do {
+       ibv_poll_cq(cq, 1, &wc);
+    } while (wc.status == IBV_WC_SUCCESS && wc.opcode != IBV_WC_RECV);
+
+    struct calc_req *req = buf;
+    uint32_t a = ntohl(req->a), b = ntohl(req->b), op = ntohl(req->op);
+    uint32_t result = 0;
+    switch (op) {
+      case 0: result = a + b; break;
+      case 1: result = a - b; break;
+      case 2: result = a * b; break;
+      case 3: result = (b!=0)? a / b : 0; break;
+      default: result = 0;
+    }
+    printf("Computed: %u %s %u = %u\n", a,
+           (op==0?"+":op==1?"-":op==2?"*":"/"), b, result);
+
+    // Prepare and send the response
+    struct calc_resp resp = { htonl(result) };
+    memcpy(buf, &resp, sizeof(resp));
+    sge.length = sizeof(resp);
+    struct ibv_send_wr send_wr = {
+      .wr_id = 2, .sg_list = &sge, .num_sge = 1,
+      .opcode = IBV_WR_SEND, .send_flags = IBV_SEND_SIGNALED
+    };
+    struct ibv_send_wr *bad_send;
+    ibv_post_send(conn_id->qp, &send_wr, &bad_send);
+
+    // Poll for send completion
+    do {
+       ibv_poll_cq(cq, 1, &wc);
+    } while (wc.status == IBV_WC_SUCCESS && wc.opcode != IBV_WC_SEND);
+
+    printf("Response sent\n");
+
+    // Cleanup
+    rdma_disconnect(conn_id);
+    ibv_dereg_mr(mr);
+    free(buf);
+    rdma_destroy_qp(conn_id);
+    ibv_destroy_cq(cq);
+    ibv_dealloc_pd(pd);
+    rdma_destroy_id(conn_id);
+    rdma_destroy_event_channel(ec);
     return 0;
 }
